11aba676ce3218d057601adc81e623b814e87a5f
Julio Romero <julio_ale21@hotmail.com>
2021-07-13 12:31:11 -0300

    Revert "feat(drag-and-drop): added user can drag and drop items results"
    This reverts commit b1bea8150f061bb18c458e4cf41d71b1cce9e5be.

--- src/components/ResultItem/ResultItem.tsx
+++ src/components/ResultItem/ResultItem.tsx
@@ -1,28 +1,17 @@
 import React from "react";
-import { Draggable } from "react-beautiful-dnd";
 import "./index.css";
 
 interface Props {
   title: string;
   value: number;
-  index: number;
 }
-const ResultItem: React.FC<Props> = ({ title, value, index }) => {
+const ResultItem: React.FC<Props> = ({ title, value }) => {
   return (
-    <Draggable draggableId={String(index)} index={index}>
-      {(provided) => (
-        <li
-          className="result"
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          ref={provided.innerRef}
-        >
-          <p>
-            {title}: <span>{value}</span>
-          </p>
-        </li>
-      )}
-    </Draggable>
+    <li className="result">
+      <p>
+        {title}: <span>{value}</span>
+      </p>
+    </li>
   );
 };
 
