0b71dcaf1424490eb47c8f49c42ef697f07cbb7d
Julio Romero <julio_ale21@hotmail.com>
2021-07-13 23:17:14 -0300

    chore: added header and footer to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Calculator from "./components/Calculator";
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1 className="title">Rooftop Custom Calculator</h1>
       <Calculator />
+      <Footer />
     </div>
   );
 }
