96726543b40479acb5a40bf1404fb9124cb4cff3
Julio Romero <julio_ale21@hotmail.com>
2021-07-13 21:16:59 -0300

    chore: added logic to calculator component

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,6 +1,8 @@
 import React, { useMemo, useState } from "react";
 import Display from "../Display";
+import Keypad from "../Keypad";
 import ResultList from "../ResultList";
+import "./index.css";
 
 interface Result {
   operation: string;
@@ -8,11 +10,15 @@ interface Result {
 }
 
 const Calculator: React.FC = () => {
-  const [input1, setInput1] = useState(0);
-  const [input2, setInput2] = useState(0);
+  const [input1, setInput1] = useState("0");
+  const [input2, setInput2] = useState("0");
   const [results, setResults] = useState([] as Result[]);
+  const [activeDisplay, setActiveDisplay] = useState("display1");
 
   useMemo(() => {
+    if (input1.charAt(input1.length - 1) === ".") return;
+    if (input2.charAt(input2.length - 1) === ".") return;
+
     const results: Result[] = [
       {
         operation: "Suma",
@@ -24,19 +30,19 @@ const Calculator: React.FC = () => {
       },
       {
         operation: "Multiplicación",
-        value: input1 * input2,
+        value: Number(input1) * Number(input2),
       },
       {
         operation: "División",
-        value: input1 / input2,
+        value: Number(input1) / Number(input2),
       },
       {
         operation: "Raiz",
-        value: Math.pow(input1, 1 / input2),
+        value: Math.pow(Number(input1), 1 / Number(input2)),
       },
       {
         operation: "Potencia",
-        value: Math.pow(input1, input2),
+        value: Math.pow(Number(input1), Number(input2)),
       },
     ];
 
@@ -44,7 +50,15 @@ const Calculator: React.FC = () => {
   }, [input1, input2]);
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const value = parseFloat(e.currentTarget.value);
+    if (!isInputValid(e.currentTarget.value.replace(".", ""))) return;
+
+    const array = Array.from(e.currentTarget.value);
+
+    const dotsCount = array.filter((item) => item === ".").length;
+
+    if (dotsCount > 1) return;
+
+    const value = e.currentTarget.value;
 
     if (e.currentTarget.id === "display1") {
       setInput1(value);
@@ -57,32 +71,75 @@ const Calculator: React.FC = () => {
     if (!isInputValid(e.currentTarget.value)) return;
 
     if (e.currentTarget.id === "display1") {
-      setInput1(0);
+      setInput1("0");
     } else if (e.currentTarget.id === "display2") {
-      setInput2(0);
+      setInput2("0");
+    }
+  };
+
+  const handleFocus = (e: React.FormEvent<HTMLInputElement>) => {
+    setActiveDisplay(e.currentTarget.id);
+  };
+
+  const handleClick = (value: string) => {
+    if (value !== "." && !isInputValid(value)) return;
+
+    if (activeDisplay === "display1") {
+      if (value === "." && input1.toString().includes(".")) {
+        return;
+      }
+
+      if (input1 === "0" && value !== ".") {
+        setInput1(value);
+
+        return;
+      }
+      let newValue = input1 + value;
+
+      setInput1(newValue);
+    } else if (activeDisplay === "display2") {
+      const newValue = input2 + value;
+
+      if (input2 === "0" && value !== ".") {
+        setInput2(value);
+
+        return;
+      }
+
+      if (value === "." && input2.toString().includes(".")) {
+        return;
+      }
+
+      setInput2(newValue);
     }
   };
 
   const isInputValid = (input: string) => {
-    let i = new Number(input);
+    let i = Number(input);
 
     return !Number.isNaN(i);
   };
 
   return (
     <div>
-      <Display
-        handleChange={handleChange}
-        handleClear={handleClearButton}
-        id="display1"
-        value={input1}
-      />
-      <Display
-        handleChange={handleChange}
-        handleClear={handleClearButton}
-        id="display2"
-        value={input2}
-      />
+      <div className="calculator">
+        <Display
+          handleChange={handleChange}
+          handleClear={handleClearButton}
+          handleFocus={handleFocus}
+          id="display1"
+          value={input1}
+        />
+        <Display
+          handleChange={handleChange}
+          handleClear={handleClearButton}
+          handleFocus={handleFocus}
+          id="display2"
+          value={input2}
+        />
+        <Keypad onClickEvent={handleClick} />
+      </div>
+
       <ResultList results={results} />
     </div>
   );
